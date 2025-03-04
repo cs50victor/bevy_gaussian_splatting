fb9f376d61a504c764dbac266079c7a3ea2e6a62
cs50victor <52110451+cs50victor@users.noreply.github.com>
2025-03-04 23:46:06 +0000

    Deploying to www from @ cs50victor/bevy_gaussian_splatting@8be87d761f8b021e9224f081aa34d3fe609f16c6 🚀

---
+++ out/bevy_gaussian_splatting_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure7390_externref_shim: (a: number, b: number, c: any) => void;
+export const closure68542_externref_shim: (a: number, b: number, c: any) => void;
+export const closure68560_externref_shim: (a: number, b: number, c: any) => void;
+export const closure68769_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h09174c957d09b835: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2373fe5a8651a868: (a: number, b: number, c: number) => void;
+export const closure68783_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
